a59ded53f69fb06cc5190626088949893e4bd549
root <root@pop-os.localdomain>
2021-12-15 18:07:18 -0700

    removed node_moudles

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return(
+        <div>
+            <h1>Welcome to Lefan's portfolio server</h1>
+        </div>
+    )
 }
-
-export default App;
