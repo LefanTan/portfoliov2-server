0d6555954ceb8de8b8df9ccfcfb16f53d9e8d3d2
LefanTan <tan6@ualberta.ca>
2022-02-01 02:53:22 +0000

    validation

--- api/middleware/project.validation.ts
+++ api/middleware/project.validation.ts
@@ -3,7 +3,7 @@ import { check } from "express-validator";
 // Make sure types are correct for project
 export const projectValidate = [
   check("title").isString().exists().withMessage("Title is required"),
-  check("description")
+  check("shortDescription")
     .isString()
     .exists()
     .withMessage("Description is required"),
